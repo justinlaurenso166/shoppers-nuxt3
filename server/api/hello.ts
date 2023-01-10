6bdbdb2701f6205c24186543bf7138fb542c017e
JustinLaurenso <justin.laurenso166@gmail.com>
2023-01-10 09:18:21 +0700

    try to add middleware

--- server/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter, useBase } from 'h3';
-
-const router = createRouter();
-
-router.get('/', () => 'Hello World');
-
-export default useBase('/api/test', router.handler);
--- server/middleware/api.ts
+++ server/middleware/api.ts
@@ -1,8 +1,9 @@
 import { createRouter, defineEventHandler, useBase } from 'h3';
 import cart from "../api/cart"
+import products from "../api/products"
 
 const router = createRouter();
 router.get('/hello', cart);
-router.get('/hello/world', defineEventHandler(event => 'Hello World'));
+router.get('/hello/world', products);
 
 export default useBase('/api', router.handler);
